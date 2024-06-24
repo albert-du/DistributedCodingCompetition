8cc1e51eae2ed52e0a9117f3f652d3c5d8f4f5dd
Albert Du <52804499+albert-du@users.noreply.github.com>
2024-06-24 16:09:25 -0700

    package manager

--- DistributedCodingCompetition.ExecRunner/Controllers/ManagementController.cs
+++ DistributedCodingCompetition.ExecRunner/Controllers/ManagementController.cs
@@ -28,6 +28,83 @@ public class ManagementController(IConfiguration configuration, HttpClient httpC
         };
     }
 
+    [HttpGet("packages")]
+    public async Task<IActionResult> GetPackagesAsync(string key)
+    {
+        if (key != configuration["Key"])
+            return Unauthorized();
+
+        var packages = await httpClient.GetFromJsonAsync<IReadOnlyList<Package>>(configuration["Piston"] + "/api/v2/packages") ?? [];
+
+        return Ok(packages.Where(x => x.Installed).Select(x => $"{x.Name}={x.Version}").Where(x => !string.IsNullOrWhiteSpace(x)));
+    }
+
+    [HttpGet("available")]
+    public async Task<IActionResult> GetAvailablePackagesAsync(string key)
+    {
+        if (key != configuration["Key"])
+            return Unauthorized();
+
+        var packages = await httpClient.GetFromJsonAsync<IReadOnlyList<Package>>(configuration["Piston"] + "/api/v2/packages") ?? [];
+
+        return Ok(packages.Select(x => $"{x.Name}={x.Version}").Where(x => !string.IsNullOrWhiteSpace(x)));
+    }
+
+    [HttpPost("packages")]
+    public async Task<IActionResult> InstallPackagesAsync(string key, [FromBody] IReadOnlyList<string> spec)
+    {
+        if (key != configuration["Key"])
+            return Unauthorized();
+        var lines = spec.Where(x => !string.IsNullOrWhiteSpace(x));
+        if (!lines.All(x => x.Split('=').Length == 2))
+            return BadRequest("Bad Spec");
+        var oldSpec = (await httpClient.GetFromJsonAsync<IReadOnlyList<Package>>(configuration["Piston"] + "/api/v2/packages") ?? []).Where(x => x.Installed).Select(x => $"{x.Name}={x.Version}").Where(x => !string.IsNullOrWhiteSpace(x));
+        HashSet<string> removed = new(oldSpec);
+        removed.ExceptWith(lines);
+        List<string> response = [];
+        // Uninstall removed packages
+        foreach (var package in removed)
+        {
+            var tokens = package.Split('=');
+            var name = tokens[0];
+            var version = tokens[1];
+            using HttpRequestMessage message = new()
+            {
+                Method = HttpMethod.Delete,
+                RequestUri = new(configuration["Piston"] + "/api/v2/packages"),
+                Content = new StringContent($$"""{ "language": "{{name}}", "version": "{{version}}"}""", Encoding.UTF8, "application/json")
+            };
+            var resp = await httpClient.SendAsync(message);
+            if (resp.IsSuccessStatusCode)
+                response.Add($"Removed {name}={version}");
+            else
+                response.Add($"ERROR: Could not remove {name}={version}; {await resp.Content.ReadAsStringAsync()}");
+        }
+
+        HashSet<string> installed = new(lines);
+        installed.ExceptWith(oldSpec);
+        // Install new packages
+        foreach (var package in installed)
+        {
+            var tokens = package.Split('=');
+            var name = tokens[0];
+            var version = tokens[1];
+            using HttpRequestMessage message = new()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new(configuration["Piston"] + "/api/v2/packages"),
+                Content = new StringContent($$"""{ "language": "{{name}}", "version": "{{version}}"}""", System.Text.Encoding.UTF8, "application/json")
+
+            };
+            var resp = await httpClient.SendAsync(message);
+            if (resp.IsSuccessStatusCode)
+                response.Add($"Installed {name}={version}");
+            else
+                response.Add($"ERROR: Could not install {name}={version}; {await resp.Content.ReadAsStringAsync()}");
+        }
+        return Ok(response.Count is 0 ? "Already to spec" : string.Join('\n', response));
+    }
+
     private static string SystemInfo()
     {
         StringBuilder sb = new();
