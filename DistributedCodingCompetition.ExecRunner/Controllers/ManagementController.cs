68213a949bfb06aeade0003c18e167516d64079d
Albert Du <52804499+albert-du@users.noreply.github.com>
2024-06-24 15:08:33 -0700

    Working API access to piston instance

--- DistributedCodingCompetition.ExecRunner/Controllers/ManagementController.cs
+++ DistributedCodingCompetition.ExecRunner/Controllers/ManagementController.cs
@@ -3,22 +3,27 @@
 using Microsoft.AspNetCore.Mvc;
 using DistributedCodingCompetition.ExecutionShared;
 using System.Text;
+using System.Net.Http;
+using System.Text.Json.Serialization;
 
 [Route("api/[controller]")]
 [ApiController]
-public class ManagementController(IConfiguration configuration) : ControllerBase
+public class ManagementController(IConfiguration configuration, HttpClient httpClient) : ControllerBase
 {
     [HttpGet]
-    public RunnerStatus Get()
+    public async Task<ActionResult<RunnerStatus>> GetAsync(string key)
     {
-        return new()
+        if (key != configuration["Key"])
+            return Unauthorized();
+        var languages = await GetLanguages();
+        return new RunnerStatus()
         {
             TimeStamp = DateTime.UtcNow,
             Version = "1.0.0",
             Healthy = true,
             Message = "Ready",
             Name = configuration["Name"] ?? "EXEC",
-            Languages = "",
+            Languages = languages,
             SystemInfo = SystemInfo()
         };
     }
@@ -33,4 +38,36 @@ public class ManagementController(IConfiguration configuration) : ControllerBase
         sb.AppendLine("Memory: " + (Environment.WorkingSet / 1024 / 1024) + "MB");
         return sb.ToString();
     }
+
+    private async Task<string> GetLanguages()
+    {
+        StringBuilder sb = new();
+        var languages = await httpClient.GetFromJsonAsync<IReadOnlyList<Language>>(configuration["Piston"] + "/api/v2/runtimes");
+        if (languages == null)
+            return string.Empty;
+        return string.Join('\n', languages.Select(l => l.Name + " " + l.Version + " " + l.Runtime));
+    }
+}
+internal record Language
+{
+    [JsonPropertyName("language")]
+    public required string Name { get; init; }
+
+    [JsonPropertyName("version")]
+    public required string Version { get; init; }
+
+    [JsonPropertyName("runtime")]
+    public string? Runtime { get; init; }
+}
+
+internal record Package
+{
+    [JsonPropertyName("language")]
+    public required string Name { get; init; }
+
+    [JsonPropertyName("language_version")]
+    public required string Version { get; init; }
+
+    [JsonPropertyName("installed")]
+    public required bool Installed { get; init; }
 }
