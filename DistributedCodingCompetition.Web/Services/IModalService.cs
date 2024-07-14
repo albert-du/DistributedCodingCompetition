44bd92c63195dc9c8a8e3ae0f59a20f254f16b79
Albert Du <52804499+albert-du@users.noreply.github.com>
2024-07-14 14:48:58 -0700

    point selection

--- DistributedCodingCompetition.Web/Services/IModalService.cs
+++ DistributedCodingCompetition.Web/Services/IModalService.cs
@@ -11,18 +11,26 @@ public interface IModalService
     /// <param name="title"></param>
     /// <param name="message"></param>
     void ShowError(string title, string message);
-    
+
     /// <summary>
     /// Show an info modal
     /// </summary>
     /// <param name="title"></param>
     /// <param name="message"></param>
     void ShowInfo(string title, string message);
-    
+
+    void AskInteger(string title, string message, int min, int max, Action<int?> onResult);
+
     /// <summary>
     /// Event that is triggered when a modal is shown
     /// </summary>
     event Action<ModalMessage> OnShow;
+    
+    
+    /// <summary>
+    /// Event that is triggered when a modal is shown
+    /// </summary>
+    event Action<IntegerModalMessage> OnShowInteger;
 
     /// <summary>
     /// Model that represents a modal message
@@ -31,6 +39,15 @@ public interface IModalService
     /// <param name="Title"></param>
     /// <param name="Message"></param>
     public sealed record ModalMessage(ModalType Type, string Title, string Message);
+
+
+    /// <summary>
+    /// Model that represents a modal message with an integer result
+    /// </summary>
+    /// <param name="Title"></param>
+    /// <param name="Message"></param>
+    /// <param name="OnResult"></param>
+    public sealed record IntegerModalMessage(string Title, string Message, int Min, int Max, Action<int?> OnResult);
 
     /// <summary>
     /// Enum that represents the type of modal
--- DistributedCodingCompetition.Web/Services/ModalService.cs
+++ DistributedCodingCompetition.Web/Services/ModalService.cs
@@ -5,6 +5,12 @@ public sealed class ModalService : IModalService
 {
     /// <inheritdoc/>
     public event Action<IModalService.ModalMessage>? OnShow;
+    public event Action<IModalService.IntegerModalMessage>? OnShowInteger;
+
+    public void AskInteger(string title, string message, int min, int max, Action<int?> onResult)
+    {
+        OnShowInteger?.Invoke(new(title, message, min, max, onResult));
+    }
 
     /// <inheritdoc/>
     public void ShowError(string title, string message) =>
